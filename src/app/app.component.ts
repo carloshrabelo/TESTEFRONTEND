8fb36f0288becc4800e51f17f08ca762182fa8c9
carlos.oliveira <carlos.oliveira@altran-ais.com.br>
2017-12-11 21:13:08 -0200

    add: Mock Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { ClienteService } from './sevices/clientes-service';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'app';
+  clientes = [];
+  clienteSelecionado;
+
+  constructor(private clienteService: ClienteService) {
+  }
+
+  ngOnInit(): void {
+    this.clienteService.getClientes().then(response => this.clientes = response );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { BigNumberComponent } from './components/big-number/big-number.component';
 import { AdicionarNotaComponent } from './modules/adicionar-nota/adicionar-nota.component';
 import { CadastrarNotasComponent } from './modules/cadastrar-notas/cadastrar-notas.component';
+
+import { ClienteService } from './sevices/clientes-service';
+
 
 @NgModule({
   declarations: [
@@ -14,11 +18,13 @@ import { CadastrarNotasComponent } from './modules/cadastrar-notas/cadastrar-not
     CadastrarNotasComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,HttpModule
   ],
   providers: [
-    { provide: LOCALE_ID, useValue: 'pt-BR'}
+    { provide: LOCALE_ID, useValue: 'pt-BR'},
+    ClienteService
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+ }
