103b6ae5288addd310daf205bb482c6d1099079b
carlos.oliveira <carlos.oliveira@altran-ais.com.br>
2017-12-11 17:12:16 -0200

    add: modulo de adicionar notas fix: Ajustado o prefix dinamico do cli, ajustes no css e na arquitetura css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BigNumberComponent } from './components/big-number/big-number.component';
+import { AdicionarNotaComponent } from './modules/adicionar-nota/adicionar-nota.component';
+import { TextMaskModule } from 'angular2-text-mask';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BigNumberComponent
+    BigNumberComponent,
+    AdicionarNotaComponent
   ],
   imports: [
     BrowserModule
